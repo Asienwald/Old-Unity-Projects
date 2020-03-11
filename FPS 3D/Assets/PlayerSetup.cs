97896faea6dfffc3a6185e6f652da4d6613839ff
KW <45307694+Asienwald@users.noreply.github.com>
2020-03-12 00:42:16 +0800

    Added Multiplayer using Mirror!

---
+++ FPS 3D/Assets/PlayerSetup.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    Camera sceneCam;
+
+    private void Start()
+    {
+        // if this obj not controlled by system
+        // disable all components not controlled by local player so inputs dont clash
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            // disable main cam when player join
+            sceneCam = Camera.main;
+            if(sceneCam != null)
+            {
+                sceneCam.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    // called when object is disabled/destroyed
+    private void OnDisable()
+    {
+        // renable scene cam when player leave
+        if(sceneCam != null)
+        {
+            sceneCam.gameObject.SetActive(true);
+        }
+    }
+}
